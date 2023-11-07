de35fa3e3f8b503c73fa7781d70ccb125c3c0a05
Apexfinity <52583315+apexfinity@users.noreply.github.com>
2023-11-06 20:47:47 -0600

    updated

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-// Example of a simple script to handle navigation clicks and scrolling
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('[role="tab"]');
+    const tabPanels = document.querySelectorAll('[role="tabpanel"]');
 
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetSection = document.querySelector(this.getAttribute('href'));
-        targetSection.scrollIntoView({ behavior: 'smooth' });
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            // Remove the selected attribute from all tabs
+            tabs.forEach(t => t.setAttribute('aria-selected', false));
+            // Hide all tab panels
+            tabPanels.forEach(panel => panel.hidden = true);
+
+            // Set the clicked tab as selected
+            tab.setAttribute('aria-selected', true);
+            // Show the associated tabPanel
+            const id = tab.getAttribute('aria-controls');
+            const tabPanel = document.getElementById(id);
+            tabPanel.hidden = false;
+        });
     });
 });
